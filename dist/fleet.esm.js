39a0dd433187be32cac952d3747c549fd859b1f5
2113ic <3072861626@qq.com>
2022-07-19 11:50:16 +0800

    v0.1.5

--- dist/fleet.esm.js
+++ dist/fleet.esm.js
@@ -1019,7 +1019,7 @@ export default (function (global) {
       this.$clearFlag(filters, this.flag);
       this.$clearFlag(this.tabs, this.flag);
       target.classList.add(this.flag);
-      tabs.classList.add(this.flag);
+      this.tabs.classList.add(this.flag);
     }
 
     initParams(tabs, flag) {
